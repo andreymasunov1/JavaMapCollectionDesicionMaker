ffa4aad19d00caf4c325f5b49a2b85779fac1503
Andrey Masunov <andrey.masunov@dci-student.org>
2024-02-08 14:38:59 +0100

    added solution for the task

---
+++ script.js
@@ -0,0 +1,90 @@
+function showMapSection() {
+    document.getElementById("start").style.display = "none";
+    document.getElementById("mapSection").style.display = "flex";
+}
+
+function showValuesSection() {
+    document.getElementById("start").style.display = "none";
+    document.getElementById("valuesSection").style.display = "flex";
+}
+
+function showHashMapSection() {
+    document.getElementById("mapSection").style.display = "none";
+    document.getElementById("hashMapSection").style.display = "flex";
+}
+
+function showTreeMapSection() {
+    document.getElementById("mapSection").style.display = "none";
+    document.getElementById("treeMapSection").style.display = "flex";
+}
+
+function showLinkedHashMapSection() {
+    document.getElementById("treeMapSection").style.display = "none";
+    document.getElementById("linkedHashMapSection").style.display = "flex";
+}
+
+function showSortedTreeMapSection() {
+    document.getElementById("treeMapSection").style.display = "none";
+    document.getElementById("sortedTreeMapSection").style.display = "flex";
+}
+
+function showInsertRemovalSection() {
+    document.getElementById("searchElementsSection").style.display = "none";
+    document.getElementById("valuesSection").style.display = "none";
+    document.getElementById("insertRemovalSection").style.display = "flex";
+}
+function showHashSetDecision() {
+    document.getElementById("isOrderImportantSection").style.display = "none";
+    document.getElementById("hashSetDecision").style.display = "flex";
+}
+
+function showArrayListDecision() {
+    document.getElementById("insertRemovalSection").style.display = "none";
+    document.getElementById("arrayListDecision").style.display = "flex";
+}
+
+function showLinkedListDecision() {
+    document.getElementById("insertRemovalSection").style.display = "none";
+    document.getElementById("linkedListDecision").style.display = "flex";
+}
+
+function showArrayListSection() {
+    document.getElementById("valuesSection").style.display = "none";
+    document.getElementById("arrayListSection").style.display = "flex";
+}
+
+function showSearchElementsSection() {
+    document.getElementById("valuesSection").style.display = "none";
+    document.getElementById("searchElementsSection").style.display = "flex";
+}
+
+function showIsOrderImportantSection() {
+    document.getElementById("searchElementsSection").style.display = "none";
+    document.getElementById("isOrderImportantSection").style.display = "flex";
+}
+
+function showOrderSortedSection() {
+    document.getElementById("isOrderImportantSection").style.display = "none";
+    document.getElementById("orderSortedSection").style.display = "flex";
+}
+
+function showLinkedListSection() {
+    document.getElementById("hashSetSection").style.display = "none";
+    document.getElementById("linkedListSection").style.display = "flex";
+}
+
+linkedListDecision
+function showLinkedListSection() {
+    document.getElementById("hashSetSection").style.display = "none";
+    document.getElementById("linkedListDecision").style.display = "flex";
+}
+
+function showTreeSetSection() {
+    document.getElementById("orderSortedSection").style.display = "none";
+    document.getElementById("treeSetSection").style.display = "flex";
+}
+
+function showLinkedHashSetSection() {
+    document.getElementById("orderSortedSection").style.display = "none";
+    document.getElementById("linkedHashSetSection").style.display = "flex";
+}
